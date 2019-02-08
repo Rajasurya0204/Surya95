7e9ba34b9d9b356c20ad0bbcde6aeeb59166be97
Rajasurya0204 <47408513+Rajasurya0204@users.noreply.github.com>
2019-02-08 09:57:45 +0530

    Update internarms.py

--- internarms.py
+++ internarms.py
@@ -1,11 +1,11 @@
-lower, upper = map(int, input().split()) 
-for num in range(lower, upper + 1):
-   order = len(str(num))
+a, b = map(int, input().split()) 
+for u in range(a, b + 1):
+   order = len(str(u))
    sum = 0
-   temp = num
+   temp = u
    while temp > 0:
        digit = temp % 10
-       sum += digit ** order
+       u += digit ** order
        temp //= 10
-   if num == sum:
+   if u == sum:
        print(num)
