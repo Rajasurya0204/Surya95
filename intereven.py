6c114d038ea8e5e4fd9fefd9d6780fb76fb6578a
Rajasurya0204 <47408513+Rajasurya0204@users.noreply.github.com>
2019-02-08 09:34:41 +0530

    Update intereven.py

--- intereven.py
+++ intereven.py
@@ -1,5 +1,4 @@
-s = int(input())
-e = int(input())
-for a in range(s, e + 1): 
-           if (a % 2) == 0:
-               print (a)
+start,end=map(int,input().split())
+for a in range(start+1, end): 
+           if a % 2 == 0:
+               print (a,end=" ")
